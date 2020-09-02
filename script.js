bd07fc57c6ee5155df36a9abc5a95407d4308b0c
Mike Bleier <mbleier@gmail.com>
2020-09-02 09:49:25 -0500

    all done except fading to black functionality

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
 //     Create 16 x 16 grid of square divs within a container created in the HTML
-// First, create one div and test
 createGrid(16)
-function createGrid(gridsize){
-    // let gridsize = 5
-    document.getElementById("sketch-container").innerHTML=""
-    for (let i = 0; i < gridsize*gridsize; i++) {
-        var div = document.createElement("div");
-        div.style.width =`${100/gridsize}%`;
-        div.style.height =`${100/gridsize}%`;
-        div.style.color = "white";
-        div.addEventListener("mouseover", function (event) {
+function createGrid(gridsize) {
+    document.getElementById("sketch-container").innerHTML = ""
+    for (let i = 0; i < gridsize * gridsize; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.style.width = `${100 / gridsize}%`;
+        newDiv.style.height = `${100 / gridsize}%`;
+        newDiv.style.color = "white";
+        newDiv.addEventListener("mouseover", function (event) {
             console.log(event.target.style.backgroundColor)
-            event.target.style.backgroundColor = getColor()
+            for (i = 0; i < 10; i++) {
+                if (event.target.style.backgroundColor != false) {
+                    let newRGB = randomColor.replace(rgb1, (rgb1 * .9)).replace(rgb2, (rgb2 * .9)).replace(rgb3, (rgb3 * .9))
+                    // let rgb = event.target.style.backgroundColor
+                    // let newRGB = rgb.replace(/\d/g, `0`);
+                    event.target.style.backgroundColor = newRGB
+                } else {
+                    event.target.style.backgroundColor = getColor()
+                }
+            }
         })
-        document.getElementById("sketch-container").appendChild(div);
+        document.getElementById("sketch-container").appendChild(newDiv);
     }
 }
+let rgb1 = 0
+let rgb2 = 0
+let rgb3 = 0
+let rgbARR = [rgb1, rgb2, rgb3]
+let randomColor = ""
 
 function getColor() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    return "#" + randomColor;
+    rgb1 = Math.floor(Math.random() * 255)
+    rgb2 = Math.floor(Math.random() * 255)
+    rgb3 = Math.floor(Math.random() * 255)
+    randomColor = "rgb(" + rgb1 + ", " + rgb2 + ", " + rgb3 + ")"
+    console.log(randomColor)
+    return randomColor
+    // const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    // return "#" + randomColor;
 }
-document.getElementById("reset-grid").addEventListener("click", function(){
-    var size = prompt("How many boxes in the grid?")
+document.getElementById("reset-grid").addEventListener("click", function () {
+    var size = prompt("How many boxes in the grid? (Enter a number 2 - 100)")
     let intSize = parseInt(size)
     createGrid(intSize)
 })

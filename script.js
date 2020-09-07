8968e76617a3de7855e74d0d28ac943032036cca
Mike Bleier <mbleier@gmail.com>
2020-09-07 09:34:33 -0500

    final commit

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-//     Create 16 x 16 grid of square divs within a container created in the HTML
+// Create 16 x 16 grid of square divs within a container created in the HTML
 createGrid(16)
 function createGrid(gridsize) {
+    // first, clear out the container
     document.getElementById("sketch-container").innerHTML = ""
+    // for loop creates divs based on "gridsize" input
     for (let i = 0; i < gridsize * gridsize; i++) {
         var newDiv = document.createElement("div");
+        // set the size and color of each newly created div
         newDiv.style.width = `${100 / gridsize}%`;
         newDiv.style.height = `${100 / gridsize}%`;
         newDiv.style.color = "white";
+        // Add event listener to listen for mouseover
         newDiv.addEventListener("mouseover", function (event) {
-            console.log(event.target.style.backgroundColor)
-            for (i = 0; i < 10; i++) {
-                if (event.target.style.backgroundColor != false) {
-                    let newRGB = randomColor.replace(rgb1, (rgb1 * .9)).replace(rgb2, (rgb2 * .9)).replace(rgb3, (rgb3 * .9))
-                    // let rgb = event.target.style.backgroundColor
-                    // let newRGB = rgb.replace(/\d/g, `0`);
-                    event.target.style.backgroundColor = newRGB
-                } else {
-                    event.target.style.backgroundColor = getColor()
-                }
+            // If no background color exists, assign one. If one exists, use RegEx to find numbers in the string and assign them to an array (matches)
+            if (event.target.style.backgroundColor != false) {
+                let oldRGB = event.target.style.backgroundColor
+                let matches = oldRGB.match(/[0-9]{1,3}/g)
+                console.log(matches)
+                // Use .map function to cycle through the array and replace each number with a lower number [I chose 80% of the original number], which will eventually turn the div black (i.e. will get close enough to RGB 0,0,0 to appear black)
+                let newRGB = matches.map(function (item) {
+                    return item * .8
+                })
+                event.target.style.backgroundColor = "rgb(" + newRGB[0] + ", " + newRGB[1] + ", " + newRGB[2] + ")"
+            } else {
+                event.target.style.backgroundColor = getColor()
             }
+            // }
         })
         document.getElementById("sketch-container").appendChild(newDiv);
     }
 }
-let rgb1 = 0
-let rgb2 = 0
-let rgb3 = 0
-let rgbARR = [rgb1, rgb2, rgb3]
-let randomColor = ""
-
+// function to pick a random RGB color
 function getColor() {
     rgb1 = Math.floor(Math.random() * 255)
     rgb2 = Math.floor(Math.random() * 255)
@@ -36,20 +38,10 @@ function getColor() {
     randomColor = "rgb(" + rgb1 + ", " + rgb2 + ", " + rgb3 + ")"
     console.log(randomColor)
     return randomColor
-    // const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    // return "#" + randomColor;
 }
+// add functionality for when button is clicked
 document.getElementById("reset-grid").addEventListener("click", function () {
     var size = prompt("How many boxes in the grid? (Enter a number 2 - 100)")
     let intSize = parseInt(size)
     createGrid(intSize)
 })
-//next, use a loop to create 16
-
-// Add a button to the top of the screen which will clear the current grid and send the user a popup asking for how many squares per side to make the new grid. Once entered the new grid should be generated in the same total space as before (e.g. 960px wide) and now you’ve got a new sketch pad.
-
-// HINT: Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
-// HINT: Also check out prompts
-// HINT: You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used
-
-// (Optional): Instead of just changing the color of your grid from black to white (for example) have each pass through it with the mouse change to a completely random RGB value. Then try having each pass just add another 10% of black to it so that only after 10 passes is the square completely black.
